b837c94b240c2a187a79f3a67c45d87b21bb19db
Ryu <sookjong3367@naver.com>
2021-05-14 00:31:35 +0900

    각 자릿수 합 구하기

---
+++ src/no9/Main.java
@@ -0,0 +1,14 @@
+package no9;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 1242;
+        int sum = 0;
+
+        // 1232 >> 1의 자리를 구함 =>123 => 12 => 1
+        // 몫 <-> 나머지
+
+
+
+    }
+};
