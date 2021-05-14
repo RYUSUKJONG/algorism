19a40166a99b84308bcfce16b5acf5cb6a599743
Ryu <sookjong3367@naver.com>
2021-05-14 18:02:36 +0900

    별 출력 2

---
+++ src/no17/Main.java
@@ -0,0 +1,22 @@
+package no17;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+         int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if(j>=i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+};
