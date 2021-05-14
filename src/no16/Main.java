98ad845379b98c0f1e6f086c7cbff820be43f797
Ryu <sookjong3367@naver.com>
2021-05-14 17:32:53 +0900

    별 출력 1

---
+++ src/no16/Main.java
@@ -0,0 +1,14 @@
+package no16;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 6;
+        
+        for(int i=0; i<n; i++){
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+};
