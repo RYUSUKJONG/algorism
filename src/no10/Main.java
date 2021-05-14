66af8b36e0a2c2be510590b9a4dab2189f754c96
Ryu <sookjong3367@naver.com>
2021-05-14 14:42:54 +0900

    숫자 사각형 1

---
+++ src/no10/Main.java
@@ -0,0 +1,16 @@
+package no10;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int j=0; j<n; j++){
+            for(int i=0; i<n; i++){
+                System.out.printf("%3d", j*n+i+1);
+            };
+            System.out.println();
+        }
+
+    }
+};
